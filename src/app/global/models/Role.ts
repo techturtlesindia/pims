3a7eaf90af9352d2f1b0da7953ae98031965194c
unknown <sanjay.singh@techturtles.in>
2019-02-09 15:32:42 +0530

    ready

--- src/app/global/models/Role.ts
+++ src/app/global/models/Role.ts
@@ -1,4 +1,8 @@
 export enum Role {
     User = 'ROLE_USER',
-    Admin = 'ROLE_ADMIN'
+    Admin = 'ROLE_ADMIN',
+    Pm='ROLE_PM',
+    Checker='ROLE_CHECKER',
+	  Employee='ROLE_EMPLOYEE',
+	  Maker='ROLE_MAKER',
   }
